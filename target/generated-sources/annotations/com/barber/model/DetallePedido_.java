b7b76be069ae674c1f4c4aedf422d5dda73cb2f9
jhoann1323 <johansalguero35@gmail.com>
2021-09-11 10:18:02 -0500

    push base

--- target/generated-sources/annotations/com/barber/model/DetallePedido_.java
+++ target/generated-sources/annotations/com/barber/model/DetallePedido_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-09-10T16:42:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-09-11T01:42:00")
 @StaticMetamodel(DetallePedido.class)
 public class DetallePedido_ { 
 
--- target/generated-sources/annotations/com/barber/model/Usuario_.java
+++ target/generated-sources/annotations/com/barber/model/Usuario_.java
@@ -12,7 +12,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-09-10T16:42:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-09-11T01:42:00")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
